be0746ccc345a6d56761f7d8346cc55103025bc5
Pard33p <sainiprince424@gmail.com>
2018-08-28 19:02:53 +0530

    added demo git line

--- githubDemo/src/githubDemo/Demo.java
+++ githubDemo/src/githubDemo/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Demo github.com");
 		System.out.println("new line");
+		System.out.println("Demo git");
 		
 	}
 
